069834616a0eb142ddd19003ba14532e15fec1b4
Diego Moreno <diegokarimex@gmail.com>
2016-11-04 23:26:50 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+NUCLEO-F072RB_flash_eeprom (PORTUGUÊS)
+==============
+- Projeto    : NUCLEO-F072RB_flash_eeprom
+- MCU        : STM32F072RB
+- Copyright  : Atribuição (CC BY 4.0) Diego Moreno
+- Descrição  : Flash como EEPROM. Grava 6 bytes e depois lê os 6 bytes.
+
+Do que você precisa:
+--------------
+# **Software**
+- IAR Embedded Workbench for ARM (EWARM) – IDE e Compilador (32 KB de limitação de código / 16KB para Cortex-M0/M0+/M1)
+http://supp.iar.com/Download/SW/?item=EWARM-EVAL
+
+ou
+- System Workbench for STM32 (SW4STM32) – IDE e Compilador (GCC)
+http://www.openstm32.org/HomePage
+
+
+- STM32CubeMX – GUI inicializador gerador de código
+http://www.st.com/content/st_com/en/products/development-tools/software-development-tools/stm32-software-development-tools/stm32-configurators-and-code-generators/stm32cubemx.html
+- Driver USB do gravador ST-Link (Windows 7, Windows 8, Windows 10)
+http://www.st.com/web/en/catalog/tools/FM147/SC1887/PF260219?s_searchtype=keyword
+
+# **Documentação**
+- Placa de desenvolvimento: NUCLEO-F072RB
+http://www.st.com/content/st_com/en/products/evaluation-tools/product-evaluation-tools/mcu-eval-tools/stm32-mcu-eval-tools/stm32-mcu-nucleo/nucleo-f072rb.html
+- Microcontrolador: STM32F072RB
+http://www.st.com/content/st_com/en/products/microcontrollers/stm32-32-bit-arm-cortex-mcus/stm32f0-series/stm32f0x2/stm32f072rb.html
+
+---------------------------------------------------------------------------------
+
+NUCLEO-F072RB_flash_eeprom (ENGLISH)
+==============
+- Project     : NUCLEO-F072RB_flash_eeprom
+- MCU         : STM32F072RB
+- Copyright   : (CC BY 4.0) Diego Moreno
+- Description : Flash memory as EEPROM memory. Stores 6 bytes and then reads 6 bytes.
+
+What do you need:
+--------------
+# **Software**
+- IAR Embedded Workbench for ARM (EWARM) – IDE and Compiler (32 KB size-limited evaluation / 16KB for Cortex-M0/M0+/M1)
+http://supp.iar.com/Download/SW/?item=EWARM-EVAL
+
+or
+- System Workbench for STM32 (SW4STM32) – IDE and Compilador (GCC)
+http://www.openstm32.org/HomePage
+
+
+- STM32CubeMX – GUI code generator
+http://www.st.com/content/st_com/en/products/development-tools/software-development-tools/stm32-software-development-tools/stm32-configurators-and-code-generators/stm32cubemx.html
+- ST-Link USB Driver (Windows 7, Windows 8, Windows 10)
+http://www.st.com/web/en/catalog/tools/FM147/SC1887/PF260219?s_searchtype=keyword
+
+# **Documentation**
+- Development board: NUCLEO-F072RB
+http://www.st.com/content/st_com/en/products/evaluation-tools/product-evaluation-tools/mcu-eval-tools/stm32-mcu-eval-tools/stm32-mcu-nucleo/nucleo-f072rb.html
+- Microcontroller: STM32F072RB
+http://www.st.com/content/st_com/en/products/microcontrollers/stm32-32-bit-arm-cortex-mcus/stm32f0-series/stm32f0x2/stm32f072rb.html
---
+++ Src/main.c
@@ -0,0 +1,221 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f0xx_hal.h"
+#include "gpio.h"
+
+/* USER CODE BEGIN Includes */
+//------------------------------------------------------------------------------
+/* Projeto    : NUCLEO-F072RB_flash_eeprom
+ * MCU        : STM32F072RB (NUCLEO-F072RB)
+ * Copyright  : Atribuição (CC BY 4.0) Diego Moreno
+ * Versão     : 1.0.0
+ * Data	      : 10/09/2016
+ * Descrição  : Flash como EEPROM. Grava 6 bytes e depois lê os 6 bytes.
+ *		NUCLEO-F072RB Schematics - http://migre.me/tVEOR
+ */
+
+/* Includes */
+
+
+/* Defines */
+#define PAGE_63    0x0801F800
+
+//------------------------------------------------------------------------------
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint32_t Address = 0;
+uint16_t Ram_Espelho[3];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+
+  /* USER CODE BEGIN 2 */
+  
+  //Gravar
+  HAL_FLASH_Unlock();
+  FLASH_PageErase(PAGE_63);
+  HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, PAGE_63 +  0, 0x1234);
+  HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, PAGE_63 +  2, 0x5678);
+  HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, PAGE_63 +  4, 0x9ABC);
+  HAL_FLASH_Lock();
+  
+  //Ler
+  Address = PAGE_63;
+  Ram_Espelho[0] = *(__IO uint16_t*)Address;
+  Address = Address + 2;
+  Ram_Espelho[1] = *(__IO uint16_t*)Address;
+  Address = Address + 2;
+  Ram_Espelho[2] = *(__IO uint16_t*)Address;
+  
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+    
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
