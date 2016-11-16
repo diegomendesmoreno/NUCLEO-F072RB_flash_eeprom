f61a34e4a80c139919892630f514c8032054a296
Diego Moreno <diegokarimex@gmail.com>
2016-11-16 16:34:59 -0200

    Corrigindo

--- Src/main.c
+++ Src/main.c
@@ -106,11 +106,11 @@ int main(void)
   
   //Ler
   Address = PAGE_63;
-  Ram_Espelho[0] = *(__IO uint16_t*)Address;
+  Ram_Espelho[0] = *(uint16_t*)Address;
   Address = Address + 2;
-  Ram_Espelho[1] = *(__IO uint16_t*)Address;
+  Ram_Espelho[1] = *(uint16_t*)Address;
   Address = Address + 2;
-  Ram_Espelho[2] = *(__IO uint16_t*)Address;
+  Ram_Espelho[2] = *(uint16_t*)Address;
   
   /* USER CODE END 2 */
 
@@ -137,9 +137,8 @@ void SystemClock_Config(void)
 
     /**Initializes the CPU, AHB and APB busses clocks 
     */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -150,11 +149,11 @@ void SystemClock_Config(void)
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
   {
     Error_Handler();
   }
